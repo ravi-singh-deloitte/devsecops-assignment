fe6e02d59cfe2cc6cc3c5b1cb3a0dcca621b1af1
Ravi Singh <rsingh95@deloitte.com>
2025-06-07 15:33:55 +0530

    feature:2 Q2- secure s3 bucket with encryption and security checks

---
+++ terraform/s3-secure/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "hu-devops-25-rsingh95-secure-bucket"
+
+  tags = {
+    Name        = "Secure S3 Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
